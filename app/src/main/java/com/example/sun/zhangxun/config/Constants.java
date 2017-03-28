f6aaf6d635793043e1610cc79738341b04dc9cb1
SliySun <806928336@qq.com>
2017-03-28 19:52:23 +0800

    主界面基础的搭建-逐渐丰富

--- app/src/main/java/com/example/sun/zhangxun/config/Constants.java
+++ app/src/main/java/com/example/sun/zhangxun/config/Constants.java
@@ -7,6 +7,20 @@ package com.example.sun.zhangxun.config;
 public class Constants {
 
     public static String BASE_URL = "http://v.juhe.cn/toutiao/index";
+
     public static String APP_KEY = "a611e344442a2f60d53941f21d03b9db";
+
+    public static String[] BASE_TITLES = {"头条","社会","国内","国际","娱乐","体育","军事","科技","财经","时尚"};
+
+    public static String PARAM_TOP = "top";
+    public static String PARAM_SHEHUI = "shehui";
+    public static String PARAM_GUONEI = "guonei";
+    public static String PARAM_GUOJI = "guoji";
+    public static String PARAM_YULE = "yule";
+    public static String PARAM_TIYU = "tiyu";
+    public static String PARAM_JUNSHI = "junshi";
+    public static String PARAM_KEJI = "keji";
+    public static String PARAM_CAIJING = "caijing";
+    public static String PARAM_SHISHANG = "shishang";
 
 }
---
+++ app/src/main/java/com/example/sun/zhangxun/ui/fragment/TabContentFragment.java
@@ -0,0 +1,30 @@
+package com.example.sun.zhangxun.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sun.zhangxun.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabContentFragment extends Fragment {
+
+
+    public TabContentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_content, container, false);
+    }
+
+}
